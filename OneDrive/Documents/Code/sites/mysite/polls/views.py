08b22fd3916df2a875134fffed055e744498d69d
Charles-Fitzmaurice <72311723+Charles-Fitzmaurice@users.noreply.github.com>
2020-10-12 14:55:18 +0100

    Minimized View Code
    Acheived by utilizing Generic Views from Django.

--- OneDrive/Documents/Code/sites/mysite/polls/views.py
+++ OneDrive/Documents/Code/sites/mysite/polls/views.py
@@ -1,5 +1,25 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+from django.views import generic
+from django.http import HttpResponseRedirect, HttpResponse
+from .models import Question, Choice
+
 # Create your views here.
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+    
+    def get_queryset(self):
+        """Return the last five published questions"""
+        return Question.objects.order_by('-pub_date')[:5]
+    
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+    
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s" % question_id)
